414b8882c3114712c6078d47f4f8be65dbe73f50
Suraksha Krishna Moolya <surakshamoolya77@gmail.com>
2022-03-21 21:28:36 +0530

    changes made

--- public/notification.php
+++ public/notification.php
@@ -4,6 +4,7 @@
  use PHPMailer\PHPMailer\PHPMailer;
  use PHPMailer\PHPMailer\SMTP;
  use PHPMailer\PHPMailer\Exception;
+ require 'credentials.php';
    function sendNotification($email,$date){
      
     
@@ -19,12 +20,12 @@
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'moolyasuraksha77@gmail.com';                     //SMTP username
-    $mail->Password   = 'mera.password';                               //SMTP password
+    $mail->Username   = EMAILID;                     //SMTP username
+    $mail->Password   = PASSWORD;                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mail->SetFrom('moolyasuraksha77@gmail.com','Co-VAX',0);
+    $mail->SetFrom(EMAILID,'Co-VAX',0);
     $mail->AddAddress($email);
     $mail->Subject= "Update for Next Dose.";
     $mail->MsgHTML($message_body);
@@ -54,12 +55,12 @@
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'moolyasuraksha77@gmail.com';                     //SMTP username
-    $mail->Password   = 'mera.password';                               //SMTP password
+    $mail->Username   = EMAILID;                     //SMTP username
+    $mail->Password   = PASSWORD;                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mail->SetFrom('moolyasuraksha77@gmail.com','Co-VAX',0);
+    $mail->SetFrom(EMAILID,'Co-VAX',0);
    $mail->AddAddress($email);
    $mail->Subject= "Update for Next Dose.";
    $mail->MsgHTML($message_body);
@@ -95,12 +96,12 @@
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'moolyasuraksha77@gmail.com';                     //SMTP username
-    $mail->Password   = 'mera.password';                               //SMTP password
+    $mail->Username   = EMAILID;                     //SMTP username
+    $mail->Password   = PASSWORD;                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mail->SetFrom('moolyasuraksha77@gmail.com','Co-VAX',0);
+    $mail->SetFrom(EMAILID,'Co-VAX',0);
    $mail->AddAddress($email);
    $mail->Subject= "Update for Next Dose.";
    $mail->MsgHTML($message_body);
